9aa6f5cb9427fd109df997ff00337dda589d1f80
sound1ab <phillip-parker@outlook.com>
2018-08-27 11:44:10 +0100

    Automatic deploying through testflight and appcenter

--- ios/build/Build/Intermediates.noindex/minipopsnative.build/Debug-iphonesimulator/minipopsnative.build/Objects-normal/x86_64/AppDelegate.d
+++ ios/build/Build/Intermediates.noindex/minipopsnative.build/Debug-iphonesimulator/minipopsnative.build/Objects-normal/x86_64/AppDelegate.d
@@ -1,16 +1,19 @@
 dependencies: \
   /Users/phillipparker/Repos/minipopsnative/ios/minipopsnative/AppDelegate.m \
   /Users/phillipparker/Repos/minipopsnative/ios/minipopsnative/AppDelegate.h \
-  /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBundleURLProvider.h \
-  ../node_modules/react-native-navigation/ios/RCCManager.h \
-  /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
-  /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
-  /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTRootView.h \
+  /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/AppCenterReactNativeCrashes/AppCenterReactNativeCrashes.h \
+  /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTEventEmitter.h \
   /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridge.h \
   /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeDelegate.h \
   /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTJavaScriptLoader.h \
+  /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
+  /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
   /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTFrameUpdate.h \
   /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTInvalidating.h \
+  /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/AppCenterReactNativeAnalytics/AppCenterReactNativeAnalytics.h \
+  /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/AppCenterReactNative/AppCenterReactNative.h \
+  /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBundleURLProvider.h \
+  ../node_modules/react-native-navigation/ios/RCCManager.h \
+  /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTRootView.h \
   /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTPushNotificationManager.h \
-  /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTEventEmitter.h \
   /Users/phillipparker/Repos/minipopsnative/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTLinkingManager.h
