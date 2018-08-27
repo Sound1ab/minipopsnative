0fb8fbe0eb07ef507bbf880fc5e2ebe992ccb1fd
sound1ab <phillip-parker@outlook.com>
2018-08-27 12:25:36 +0100

    Now using react native config to handle env file

--- src/helpers/awsmobile.js
+++ src/helpers/awsmobile.js
@@ -2,40 +2,25 @@ import Amplify from 'aws-amplify'
 import { PushNotificationIOS, AsyncStorage, Linking } from 'react-native'
 import PushNotification from '@aws-amplify/pushnotification'
 import get from 'lodash/get'
-import {
-  aws_cognito_identity_pool_id,
-  aws_cognito_region,
-  aws_content_delivery,
-  aws_content_delivery_bucket,
-  aws_content_delivery_bucket_region,
-  aws_content_delivery_cloudfront,
-  aws_content_delivery_cloudfront_domain,
-  aws_project_id,
-  aws_project_name,
-  aws_project_region,
-  aws_resource_name_prefix,
-  aws_sign_in_enabled,
-  aws_user_pools,
-  aws_user_pools_id,
-  aws_user_pools_web_client_id,
-} from 'react-native-dotenv'
+import Config from 'react-native-config'
 
-const config = {
-  aws_cognito_identity_pool_id,
-  aws_cognito_region,
-  aws_content_delivery,
-  aws_content_delivery_bucket,
-  aws_content_delivery_bucket_region,
-  aws_content_delivery_cloudfront,
-  aws_content_delivery_cloudfront_domain,
-  aws_project_id,
-  aws_project_name,
-  aws_project_region,
-  aws_resource_name_prefix,
-  aws_sign_in_enabled,
-  aws_user_pools,
-  aws_user_pools_id,
-  aws_user_pools_web_client_id,
+const AWS_CONFIG = {
+  aws_cognito_identity_pool_id: Config.aws_cognito_identity_pool_id,
+  aws_cognito_region: Config.aws_cognito_region,
+  aws_content_delivery: Config.aws_content_delivery,
+  aws_content_delivery_bucket: Config.aws_content_delivery_bucket,
+  aws_content_delivery_bucket_region: Config.aws_content_delivery_bucket_region,
+  aws_content_delivery_cloudfront: Config.aws_content_delivery_cloudfront,
+  aws_content_delivery_cloudfront_domain:
+    Config.aws_content_delivery_cloudfront_domain,
+  aws_project_id: Config.aws_project_id,
+  aws_project_name: Config.aws_project_name,
+  aws_project_region: Config.aws_project_region,
+  aws_resource_name_prefix: Config.aws_resource_name_prefix,
+  aws_sign_in_enabled: Config.aws_sign_in_enabled,
+  aws_user_pools: Config.aws_user_pools,
+  aws_user_pools_id: Config.aws_user_pools_id,
+  aws_user_pools_web_client_id: Config.aws_user_pools_web_client_id,
 }
 
 let aws = null
@@ -48,7 +33,7 @@ class AwsMobile {
   }
 
   configure = () => {
-    Amplify.configure(config)
+    Amplify.configure(AWS_CONFIG)
   }
 
   setupPushNotificationListeners = async (onNotification, onRegister) => {
