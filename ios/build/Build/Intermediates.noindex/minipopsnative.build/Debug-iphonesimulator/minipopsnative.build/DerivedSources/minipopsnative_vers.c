bcac8301feab04a8ca8b7570b173b73782201ac6
sound1ab <phillip-parker@outlook.com>
2018-08-30 07:32:04 +0100

    ios

--- ios/build/Build/Intermediates.noindex/minipopsnative.build/Debug-iphonesimulator/minipopsnative.build/DerivedSources/minipopsnative_vers.c
+++ ios/build/Build/Intermediates.noindex/minipopsnative.build/Debug-iphonesimulator/minipopsnative.build/DerivedSources/minipopsnative_vers.c
@@ -1,5 +1,5 @@
  extern const unsigned char minipopsnativeVersionString[];
  extern const double minipopsnativeVersionNumber;
 
- const unsigned char minipopsnativeVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:minipopsnative  PROJECT:minipopsnative-6" "\n";
- const double minipopsnativeVersionNumber __attribute__ ((used)) = (double)6.;
+ const unsigned char minipopsnativeVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:minipopsnative  PROJECT:minipopsnative-7" "\n";
+ const double minipopsnativeVersionNumber __attribute__ ((used)) = (double)7.;
